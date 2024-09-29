0280e946f44c876524eada0b3c6135ab2c1163b5
MrBeelo <mrbeelomakris@gmail.com>
2024-09-29 13:31:19 +0300

    Test commit 2

--- Project.cs
+++ Project.cs
@@ -1,4 +1,11 @@
-using BlobGame;
-
 using (var game = new BlobGame.Main())
     game.Run();
+    
+//git status (optional, outputs all changed files)
+//git add . (to stage all files)
+//git add <file1> <file2> (to stage specific files)
+//git commit -m "Your commit message here" (to commit)
+//git push origin main (to push, with main being the branch name)
+
+
+
