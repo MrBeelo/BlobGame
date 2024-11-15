4f962a44898ac81d047d2f5652a96a5c3a499a5a
MrBeelo <mrbeelomakris@gmail.com>
2024-11-15 14:29:37 +0200

    0.27

--- obj/Debug/net8.0/win-x64/BlobGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/win-x64/BlobGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlobGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2976040b21b2380bc33892bcdf51a761bb7a708e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9550accca656865dea37a4d0e13e36c4572c678f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlobGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlobGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
